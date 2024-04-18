c93b8251ccacd0a78fdc41a4d2c7bef036c21b39
Lucas <makucas@hotmail.com>
2024-04-18 20:53:47 -0300

    adding new methods

--- server.py
+++ server.py
@@ -3,25 +3,43 @@ import threading
 import my_google
 
 def handle_request(client_socket, request):
+    data_list = my_google.load_data()
+    index_size = len(data_list)
+
     if request.lower() == "search":
         client_socket.send("Enter the search string!".encode("utf-8"))
 
         while True:
-            request = client_socket.recv(1024).decode("utf-8")
+            search_string = client_socket.recv(1024).decode("utf-8")
             # Return an index list that has the position of each founded item.
-            index_list = my_google.search(request)
+            index_list = my_google.search(search_string, data_list)
 
             if index_list:
                 composed_string = ""
                 # Iterate through each index and construct the string that contains the answer.
                 for index in index_list:
-                    title = my_google.show(index)
+                    title = my_google.show(index, data_list)
                     composed_string += title + "\n"
                 composed_string += "\nTask completed!"
                 client_socket.send(composed_string.encode("utf-8"))
                 break
             else:
                 client_socket.send("not founded, try again".encode("utf-8"))
+
+    if request.lower() == "insert":
+        client_socket.send("Enter the path to the archive".encode("utf-8"))
+
+        while True:
+            archive_path = client_socket.recv(1024).decode("utf-8")
+            if my_google.insert(archive_path, data_list, index_size):
+                index_size+=1
+                client_socket.send("file inserted".encode("utf-8"))
+            else:
+                client_socket.send("ERROR: file not inserted, try again".encode("utf-8"))
+
+
+    if request.lower() == "remove":
+        pass
 
     else:
         response = "Didn't find any option!"
